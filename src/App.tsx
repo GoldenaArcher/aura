d880963d79582762803ebd3ee83f8b7857b043b8
Zipeng <zipengchen1832@gmail.com>
2022-04-24 23:17:29 -0700

    Revert "added Input and Card components"
    This reverts commit 38ab043e3b6362132fb1a0e53efbb069b260175e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import Card from './components/Card/Card'
-import Input from './components/Input/Input'
 
 function App(): JSX.Element {
-  return <Card className='card__light'>
-    <Input />
-  </Card>;
+  return <div>hello world</div>;
 }
 
 export default App;
