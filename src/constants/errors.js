e54a2500049208507f2639b68d8ba5bca21776ff
Ronan-Codes <galv.ronan@gmail.com>
2022-04-26 13:57:40 -0400

    Prettier for error.js

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -1,12 +1,14 @@
 // pseudo error data
 let errorOptions = {
-  'lockedOneMin': 'Your account has been locked because of consecutive failed login attempts. Please try again in 30 minutes.',
-  'lockedThirtyMin': 'Your account has been locked because of consecutive failed login attempts. Please try again in 30 minutes.',
-  'invalidCredsOne': 'Invalid Credentials. Attempts Remaining: 1',
-  'invalidCredsTwo': 'Invalid Credentials. Attempts Remaining: 2',
-  'userInactive': 'User is inactive. Please contact the Admin.',
-  'invalidEmail': 'Email ID does not exist.',
-  'invalidPassword': 'Passwords do not match'
+  lockedOneMin:
+    'Your account has been locked because of consecutive failed login attempts. Please try again in 30 minutes.',
+  lockedThirtyMin:
+    'Your account has been locked because of consecutive failed login attempts. Please try again in 30 minutes.',
+  invalidCredsOne: 'Invalid Credentials. Attempts Remaining: 1',
+  invalidCredsTwo: 'Invalid Credentials. Attempts Remaining: 2',
+  userInactive: 'User is inactive. Please contact the Admin.',
+  invalidEmail: 'Email ID does not exist.',
+  invalidPassword: 'Passwords do not match',
 };
 
 export default errorOptions;
