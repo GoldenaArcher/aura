2039e935e13b675cedf89eb8546173305e44924e
Ronan-Codes <galv.ronan@gmail.com>
2022-04-26 12:47:16 -0400

    Removed redux and cleaned up

--- src/stores/reducers/index.ts
+++ src/stores/reducers/index.ts
@@ -1,16 +1,13 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducer, { AuthState } from './auth.reducer';
-import errorReducer, { ErrorState } from './error.reducer';
 
 export interface AppState {
   auth: AuthState;
-  error: ErrorState;
 }
 
 const createRootReducer = () =>
   combineReducers({
-    auth: authReducer,
-    error: errorReducer,
+    auth: authReducer
   });
 
 export default createRootReducer;
--- src/ui/error/index.tsx
+++ src/ui/error/index.tsx
@@ -1,29 +1,15 @@
 import React, { FC } from 'react';
 import { Alert } from '@mui/material';
 
-import { useSelector } from 'react-redux';
-// import { changeText, clearText } from '../../stores/actions/error.action';
-import { AppState } from '../../stores/reducers';
-import { ErrorState } from '../../stores/reducers/error.reducer';
-
 // link to error constants
   // import errorOptions from'../../constants/errors'
     // Example Usage: <Error errorType={errorType.invalidPassword.errorText}/>
-
-// import { changeText, clearText } from '../../stores/actions/error.action';
-// import { useDispatch } from 'react-redux';
-  // Redux dispatch methods
-    // dispatch(changeText(errorOptions.invalidPassword.errorText))
-    // dispatch(clearText())
 
 interface ITextInputProps {
   errorType?: string;
 }
 
 const Error: FC<ITextInputProps> = ({ errorType = '', ...rest }) => {
-  const error = useSelector<AppState, ErrorState>((state) => state.error);
-  console.log(error);
-  // const dispatch = useDispatch()
 
   return (
     <Alert
@@ -31,7 +17,7 @@ const Error: FC<ITextInputProps> = ({ errorType = '', ...rest }) => {
       severity="error"
       sx={{ bgcolor: 'white', color: 'red' }}
     >
-      {error.errorText}
+      {errorType}
     </Alert>
   );
 };
