873eb381e37abaac178d19da97bb7440fa1995c7
Zipeng <zipengchen1832@gmail.com>
2022-04-25 23:57:13 -0700

    added transparent prop to card

--- src/ui/card/index.tsx
+++ src/ui/card/index.tsx
@@ -1,7 +1,6 @@
-import React, {ReactElement} from 'react'
+import React, { ReactElement } from 'react';
 import { Card as MUICard, Paper } from '@mui/material';
-import './_card.scss'
-
+import './_card.scss';
 
 type CardPropType = {
   className?: string;
@@ -9,24 +8,32 @@ type CardPropType = {
   backgroundColor?: string;
   elevation?: number;
   shadow?: boolean;
-}
+  transparent?: boolean;
+};
 
-const Card = ({className="",children, backgroundColor="white",elevation=0,shadow=false}:CardPropType) => {
-
+const Card = ({
+  className = '',
+  children,
+  backgroundColor = 'white',
+  elevation = 0,
+  shadow = false,
+  transparent = false,
+}: CardPropType) => {
   return (
-    <Paper
-      elevation={elevation}
-    >
-      <MUICard 
-        className={`card ${shadow && "card--shadow "}` + className} 
-        style={{backgroundColor:backgroundColor}}
+    <Paper elevation={elevation}>
+      <MUICard
+        className={
+          `card ${shadow && 'card--shadow '}${
+            transparent && 'card--transparent '
+          }` + className
+        }
+        style={{ backgroundColor: backgroundColor }}
         variant="outlined"
       >
-          {children}
+        {children}
       </MUICard>
     </Paper>
-  )
-}
-
+  );
+};
 
 export default Card;
