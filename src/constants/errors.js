dc2f58335cba00b7df619518d58fd5eb8b69f031
Ronan-Codes <galv.ronan@gmail.com>
2022-04-26 13:15:15 -0400

    edited error.js

--- src/constants/errors.js
+++ src/constants/errors.js
@@ -1,31 +1,12 @@
 // pseudo error data
 let errorOptions = {
-  accountLocked: {
-    id: '1',
-    errorText: {
-      1: 'Your account has been locked because of consecutive failed login attempts. Please try again in 1 minute.',
-      30: 'Your account has been locked because of consecutive failed login attempts. Please try again in 30 minutes.',
-    },
-  },
-  invalidCredentials: {
-    id: '2',
-    errorText: {
-      1: 'Invalid Credentials. Attempts Remaining: 1',
-      2: 'Invalid Credentials. Attempts Remaining: 2',
-    },
-  },
-  userInactive: {
-    id: '3',
-    errorText: 'User is inactive. Please contact the Admin.',
-  },
-  invalidEmail: {
-    id: '4',
-    errorText: 'Email ID does not exist.',
-  },
-  invalidPassword: {
-    id: '5',
-    errorText: 'Passwords do not match',
-  },
+  'lockedOneMin': 'Your account has been locked because of consecutive failed login attempts. Please try again in 30 minutes.',
+  'lockedThirtyMin': 'Your account has been locked because of consecutive failed login attempts. Please try again in 30 minutes.',
+  'invalidCredsOne': 'Invalid Credentials. Attempts Remaining: 1',
+  'invalidCredsTwo': 'Invalid Credentials. Attempts Remaining: 2',
+  'userInactive': 'User is inactive. Please contact the Admin.',
+  'invalidEmail': 'Email ID does not exist.',
+  'invalidPassword': 'Passwords do not match'
 };
 
 export default errorOptions;
