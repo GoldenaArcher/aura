8c9a7164805169b4f338264227aa1b81aa9245a6
Ronan-Codes <galv.ronan@gmail.com>
2022-04-27 11:56:48 -0400

    New dummy data

--- src/constants/dummyData.js
+++ src/constants/dummyData.js
@@ -1,10 +1,158 @@
-// candidates
-// employees
-// jobs
-// notifications
-// onBoarding
-// organizations
-// project onbording
-// birthdays
-// work anniversaries
-// holidays
+export const employees = [
+  {
+    id: '0',
+    name: 'Srikanth',
+    position: 'Executive - US HR',
+    employeeId: '039348',
+  },
+  {
+    id: '1',
+    name: 'Sreenu K',
+    position: 'UX Designer',
+    employeeId: '039348',
+  },
+  {
+    id: '2',
+    name: 'John Smith',
+    email: 'John.smith@antra.com',
+    password: 'password',
+  },
+  {
+    id: '3',
+    name: 'Ramesh Panti',
+    email: 'rameshpanti@gmail.com',
+    password: 'password',
+  },
+];
+
+export const candidates = [
+  {
+    id: '0',
+    name: 'Srikanth N',
+    jobRole: '.Net Developer',
+    batchId: 'SEP-2020-AUG-JAVA-7',
+  },
+  {
+    id: '1',
+    name: 'Ramesh Panti',
+    jobRole: 'JAVA Developer',
+    batchId: 'SEP-2020-AUG-JAVA-7',
+  },
+];
+export const onBoarding = [
+  {
+    name: 'Haoran Li',
+    jobRole: 'Java Developer',
+    Status: 'Pending',
+    Progress: '0%',
+    'Due Date': '28 October 2020',
+  },
+  {
+    name: 'Yunxuan Sun',
+    'Job Role': 'Web Developer',
+    Status: 'Pending',
+    Progress: '90%',
+    'Due Date': '18 October 2020',
+  },
+  {
+    name: 'Haoran Li',
+    'Job Role': '.Net Developer',
+    Status: 'Pending',
+    Progress: '20%',
+    'Due Date': '23 October 2020',
+  },
+  {
+    name: 'Yunxuan Sun',
+    'Job Role': 'Web Developer',
+    Status: 'Pending',
+    Progress: '80%',
+    'Due Date': '18 October 2020',
+  },
+  {
+    name: 'Haoran Li',
+    'Job Role': '.Net Developer',
+    Status: 'Pending',
+    Progress: '20%',
+    'Due Date': '23 October 2020',
+  },
+];
+
+export const jobs = [
+  {
+    role: 'RubyRails',
+    date: '1-9 2020',
+    company: 'Apex',
+  },
+  {
+    role: 'Angular Developer',
+    date: 'Yesterday',
+    company: 'Apex',
+  },
+];
+
+export const notifications = [
+  {
+    title: 'New user request',
+    from: 'Naveen Yarragondu',
+  },
+  {
+    title: 'New Email',
+    from: 'Naveen Yarragondu',
+  },
+];
+
+export const organizations = [
+  {
+    name: 'Client Name 1',
+    city: 'New York',
+  },
+  {
+    name: 'Client Name 2',
+    city: 'Chicago',
+  },
+  {
+    name: 'Client Name 3',
+    city: 'Las Vegas',
+  },
+  {
+    name: 'Client Name 4',
+    city: 'Miami',
+  },
+  {
+    name: 'Client Name 5',
+    city: 'Boston',
+  },
+];
+
+export const projectOnboarding = [
+  {
+    id: '1',
+    name: 'Hang',
+  },
+];
+
+export const birthdays = [
+  {
+    name: 'Hang',
+    date: 'Tomorrow',
+  },
+  {
+    name: 'Teena',
+    date: 'Tomorrow',
+  },
+];
+
+export const workAnniversaries = [
+  {
+    name: 'Hang',
+    date: 'Sep 22',
+  },
+];
+
+export const holidays = [
+  {
+    name: 'Thanksgiving Day',
+    date: 'NOV 26',
+    day: 'Thursday',
+  },
+];
