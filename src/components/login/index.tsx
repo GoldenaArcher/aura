994e6959084f78512cf932e50fcfb8ba7d1d0607
GoldenaArcher <lu.han9467@gmail.com>
2022-04-26 22:58:20 -0600

    finish 2 basic card functionalities

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router';
 import { AppState } from '../../stores/reducers';
 import { AuthState } from '../../stores/reducers/auth.reducer';
+import Card, { CardBoardRadius } from '../../ui/card';
 import { isAuthed } from '../../utils/auth.util';
 
 const Login = () => {
@@ -12,7 +13,21 @@ const Login = () => {
 
   if (isLoggedIn) return <Navigate replace to="/dashboard" />;
 
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <Card
+        transparent={true}
+        borderRadius={CardBoardRadius.Large}
+      >
+        <>
+        <h2>outer card</h2>
+        <Card borderRadius={CardBoardRadius.Large}>
+          <p>inner card</p>
+        </Card>
+        </>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
